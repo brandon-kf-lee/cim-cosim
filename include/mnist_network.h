a14eea107f90447d2c64693cf6e5cbf5f5526085
Brandon Lee <bellee@chapman.edu>
2025-09-23 18:25:25 -0700

    Add MNIST image struct

--- include/mnist_network.h
+++ include/mnist_network.h
@@ -1,13 +1,22 @@
 // mnist_network.h
 
-#include <stdio.h>
+#ifndef MNIST_NETWORK_H
+#define MNIST_NETWORK_H
 
 #define MNIST_IMAGE_WIDTH 28
 #define MNIST_IMAGE_HEIGHT 28
 #define MNIST_IMAGE_SIZE MNIST_IMAGE_WIDTH * MNIST_IMAGE_HEIGHT
 #define MNIST_LABELS 10
 
+// Holds DNN weights and biases, trained on MNIST images
 typedef struct neural_network_t_ {
     float b[MNIST_LABELS];
     float W[MNIST_LABELS][MNIST_IMAGE_SIZE];
 } neural_network_t;
+
+// 28 x 28 pixel image with no byte padding
+typedef struct mnist_image_t_ {
+    uint8_t pixels[MNIST_IMAGE_SIZE];
+} __attribute__((packed)) mnist_image_t;
+
+#endif // MNIST_NETWORK_H
