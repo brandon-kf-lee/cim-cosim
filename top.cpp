d9b7d3ba68929d219c26c675e4c94c1b2cab550f
Brandon Lee <bellee@chapman.edu>
2025-10-05 16:43:30 -0700

    Add new DMA interrupt signal

--- top.cpp
+++ top.cpp
@@ -2,10 +2,11 @@
 
 #include "include/sram.h"
 #include "include/mem_controller.h"
+#include "include/mem_controller_registers.h"
 #include "include/testbench.h"
 
 int sc_main(int argc, char* argv[]) {
-    Sram sram("sram", 262144); // 256KB SRAM
+    Sram sram("sram");
     Mem_Controller mc("mc");
     Testbench tb("tb");
 
@@ -14,9 +15,17 @@ int sc_main(int argc, char* argv[]) {
     mc.i_sram_socket.bind(sram.socket); // Bind memory controller socket to SRAM socket
 
     // IRQ wiring
-    sc_core::sc_signal<bool> irq_sig("irq_sig");
-    mc.irq(irq_sig);
-    tb.irq(irq_sig);
+    sc_core::sc_signal<bool> ctrl_irq_sig("ctrl_irq_sig");
+    sc_core::sc_signal<bool> dma_irq_sig("dma_irq_sig");
+    
+    // Mem controller (CTRL) interrupts
+    // TODO: Rename irq to ctrl_irq
+    mc.irq(ctrl_irq_sig);
+    tb.irq(ctrl_irq_sig);
+    
+    // DMA interrupts
+    mc.dma_irq(dma_irq_sig);
+    tb.dma_irq(dma_irq_sig);
 
     sc_core::sc_start();
 
