ca192a7a8e969e46770072f2df57b13f6bc2f87d
Brandon Lee <brandon-kf-lee@gmail.com>
2026-03-07 12:19:31 -0800

    Remove simulated fully-parallel pass and replace with simple matrix multiplication. Timing to be computed separately

--- mem_ctrl/src/sram.cpp
+++ mem_ctrl/src/sram.cpp
@@ -82,69 +82,25 @@ void Sram::b_transport(tlm_generic_payload &trans, sc_time &delay) {
 /**
  * Perform a simulated fully-parallel forward pass of a single-layer neural network stored in SRAM
  *
- * This function reads input pixels, weights, and biases from memory, computes
- * the dot product for each neuron in parallel, and writes the resulting
- * activations back to memory. Each neuron computes its output independently,
- * and each multiplication within a neuron is also computed in parallel.
- *
- * Synchronization:
- *   - Uses sc_event and counters to ensure all multiplications and neurons
- *     complete before returning.
- *
  * Notes:
- *   - Pixel values are normalized to [0,1] by dividing by 255.
- *   - Each neuron computation and every multiplication within it spawns a separate SystemC thread.
+ *   - Pixel values are pre-normalized (before being written to device)to [0,1] by dividing by 255.
  */
 void Sram::compute_in_memory() {
-    float* weights = (float*)&mem[WEIGHT_BASE_ADDR];
-    float* bias = (float*)&mem[BIAS_BASE_ADDR];
-    uint8_t* pixels = (uint8_t*)&mem[INPUT_BASE_ADDR];
-    float* activations = (float*)&mem[OUTPUT_BASE_ADDR];
+    float *weights  = (float*)&mem[WEIGHT_BASE_ADDR];
+    float *bias     = (float*)&mem[BIAS_BASE_ADDR];
+    float *in       = (float*)&mem[INPUT_BASE_ADDR];   /* Note: Pre-normalized image pixels */
+    float *out      = (float*)&mem[OUTPUT_BASE_ADDR];
 
-    int neuron_remaining = MNIST_LABELS; // Counter + event to track how many neurons left for synchronization
-    sc_event all_done;
-
-    // Parallelized rows (propage the image through each neuron) 
-    // Copy local variables by value (except synchronization variables) into lambda
-    for(int i = 0; i < MNIST_LABELS; i++) {
-    printf("Neuron %d spawned.\n", i);
-    sc_spawn([=, &all_done, &neuron_remaining]() {
-            float sum = bias[i]; // Initialize final dot product with bias
-            float partial_mult[MNIST_IMAGE_SIZE]; // Store each intermediate pixel x weight calculation
-            int mult_remaining = MNIST_IMAGE_SIZE; // Counter + event to track how many multiplications left for synchronization
-            sc_event mult_done;
-
-            // Parallelized columns (728 pixels x 728 weights, all done simultaneously)
-            // Copy variables by reference (except j) into lambda
-            for(int j = 0; j < MNIST_IMAGE_SIZE; j++) {
-                sc_spawn([&, j]() {
-                    float normalized = (float)pixels[j] / 255.0f;
-                    partial_mult[j] = normalized * weights[i * MNIST_IMAGE_SIZE + j];
-
-                    // Decrement multiplier counter (no race conditions, SystemC guarantees exclusive non-prememptive control over data)
-                    mult_remaining--;
-                    if (mult_remaining == 0) {
-                        mult_done.notify(); // Notify when all multipliers finish
-                    }
-                });
-            }
-            wait(mult_done);
-
-            // Adder tree
-            for(int j = 0; j < MNIST_IMAGE_SIZE; j++) {
-                sum += partial_mult[j];
-            }
-            wait(SC_ZERO_TIME); // Modelled delay
-            
-            activations[i] = sum;
-
-            // Decrement neuron counter atomically
-            neuron_remaining--;
-            if (neuron_remaining == 0) {
-                all_done.notify(); // Notify when all multipliers finish
-            }
-            printf("Neuron %d finished.\n", i);
-        });
+    // Functional computation
+    for (int i = 0; i < MNIST_LABELS; i++) {
+        float sum = bias[i];
+        for (int j = 0; j < MNIST_IMAGE_SIZE; j++) {
+            sum += in[j] * weights[i * MNIST_IMAGE_SIZE + j];
+        }
+        out[i] = sum;
     }
-    wait(all_done); // All rows complete in parallel
+
+    // Timing model for a fully parallel dot-product engine and adder tree
+
+    wait(sc_time(0, SC_NS));
 }
