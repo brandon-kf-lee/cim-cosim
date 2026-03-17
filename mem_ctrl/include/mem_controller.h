8e096042883ade27e8b84fb77edcb13e1cb5a6d9
Brandon Lee <brandon-kf-lee@gmail.com>
2026-03-17 00:17:58 -0700

    Remove old references to SystemC DMA

--- mem_ctrl/include/mem_controller.h
+++ mem_ctrl/include/mem_controller.h
@@ -13,6 +13,7 @@
 #include "tlm_utils/simple_target_socket.h"
 
 #include "include/mem_controller_registers.h"
+#include "include/timing_params.h"
 
 
 SC_MODULE(Mem_Controller) { 
@@ -20,9 +21,8 @@ SC_MODULE(Mem_Controller) {
     tlm_utils::simple_target_socket<Mem_Controller> t_cpu_socket;
     tlm_utils::simple_initiator_socket<Mem_Controller> i_sram_socket;
     
-    // CTRL (memory controller) and DMA interrupt handler
+    // CTRL (memory controller)
     sc_core::sc_out<bool> irq; // TODO: rename ctrl_irq
-    // sc_core::sc_out<bool> dma_irq;
 
     SC_CTOR(Mem_Controller);
     ~Mem_Controller();
@@ -37,40 +37,22 @@ private:
     uint32_t reg_wdata{0};
     uint32_t reg_rdata{0};
     uint32_t reg_status{0};
-
-//     // DMA registers/state
-// #if HOST_64BIT
-//     uint32_t dma_src_addr_lo{0};  // Low 32 bits of source address
-//     uint32_t dma_src_addr_hi{0};  // High 32 bits of source address
-// #else
-//     uint32_t dma_src_addr{0};     // Single 32-bit source address
-// #endif
-//     uint32_t dma_control{0};
-//     uint32_t dma_len{0};
-//     uint32_t dma_dst_addr{0};
-//     uint32_t dma_status{0};
-
-    // DMA buffer
-    // uint8_t* dma_buffer;
-    //static constexpr uint32_t DMA_BUFFER_SIZE = 4096; // 4KB buffer
     
     // Events
-    // sc_core::sc_event dma_start_event;      // Triggers DMA transfer
-    // sc_core::sc_event dma_irq_update_event; // Trigger dma_irq update
     sc_core::sc_event irq_update_event;     // Trigger irq update //TODO: rename to ctrl_irq_update_event
 
     // Methods
     void ctrl_handler(tlm::tlm_generic_payload& trans, sc_core::sc_time& delay);
     void dma_handler(tlm::tlm_generic_payload& trans, sc_core::sc_time& delay);
-    
     void start_operation(sc_core::sc_time& delay);
-    // void dma_engine();
+    void prepare_sram_transaction(tlm::tlm_generic_payload& trans, 
+                                  tlm::tlm_command cmd, 
+                                  uint32_t addr, 
+                                  uint8_t* data, 
+                                  uint32_t len);
 
     // Dedicated manager threads to prevent multiple other threads directly accessing irq signal
     void irq_manager();
-    // void dma_irq_manager();
-    
-    void prepare_sram_transaction(tlm::tlm_generic_payload& trans, tlm::tlm_command cmd, uint32_t addr, uint8_t* data, uint32_t len);
 };
 
 #endif // MEM_CONTROLLER_H
